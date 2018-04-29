c0b1833e05484c36649ffe6aedb7d505968a9fc0
Thomas LaFreniere <thomas@lafreniere.co>
2018-04-28 20:49:26 -0700

    fix release vars

--- release.sh
+++ release.sh
@@ -7,10 +7,16 @@ token=`gpg -d ~/secrets/github/releaser.gpg`
 
 curl -u $username:$token https://api.github.com/user
 
-RELASE_ID=`curl -u $username:$token -d '{ "tag_name": "${tag}", "target_commitish": "master", "name": "${tag}", "body": "Terrarium release" }' -H "Content-Type: application/json" -X POST https://api.github.com/repos/laftho/terrarium/releases | jq -r '.id'`
+RELASE_ID=`curl -u $username:$token -d "{ \"tag_name\": \"${tag}\", \"target_commitish\": \"master\", \"name\": \"${tag}\", \"body\": \"Terrarium release\" }" -H "Content-Type: application/json" -X POST https://api.github.com/repos/laftho/terrarium/releases | jq -r '.id'`
+
+echo "Relase ID: ${RELEASE_ID}"
+
+echo "Uploading dxfs"
 
 curl -u $username:$token -d "@top.dxf" -H "Content-Type: image/vnd.dxf" -X POST https://uploads.github.com/repos/laftho/terrarium/releases/${RELEASE_ID}/assets?name=top.dxf
 curl -u $username:$token -d "@bottom.dxf" -H "Content-Type: image/vnd.dxf" -X POST https://uploads.github.com/repos/laftho/terrarium/releases/${RELEASE_ID}/assets?name=bottom.dxf
+
+echo "Uploading svgs"
 
 curl -u $username:$token -d "@top.svg" -H "Content-Type: application/vnd.oipf.dae.svg+xml" -X POST https://uploads.github.com/repos/laftho/terrarium/releases/${RELEASE_ID}/assets?name=top.svg
 curl -u $username:$token -d "@bottom.svg" -H "Content-Type: application/vnd.oipf.dae.svg+xml" -X POST https://uploads.github.com/repos/laftho/terrarium/releases/${RELEASE_ID}/assets?name=bottom.svg
